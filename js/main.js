f77a3857501586d639d3ee7c92d88263e28aa1ae
jona199608 <subjona@gmail.com>
2022-07-10 18:59:00 -0300

    COMMIT 3

--- js/main.js
+++ js/main.js
@@ -1,56 +1,50 @@
 let color = ['ROJA','NEGRA','AZUL'];
 let precio = ['300','250','150'];
-let talle = ['M','XL','S'];
+let talle = ['S','M','L','XL','XXL'];
 let remera = 'color de la remera es:'
 
-let comprar = prompt("comprar S (O) E-salir");
-while ( comprar != "E"){
-    if(comprar == 'S'){
-        let selecColor = prompt("seleccione color (1-roja) (2-negra) (3-azul) (O) (E-salir)");
-        {
-            if(selecColor == "1"){
-                let selecTalle = prompt("seleccionar talle (1-M) (2-XL) (3-S)")
-                if(selecTalle == "1"){
-                    console.log(`${remera}${color[0]},TALLE: ${talle[0]},TOTAL: ${precio[0]}`)
-                }else if(selecTalle == '2'){
-                    console.log(`${remera}${color[0]},TALLE: ${talle[1]},TOTAL: ${precio[1]}`)
-                }else if (selecTalle == '3'){
-                    console.log(`${remera}${color[0]},TALLE: ${talle[2]},TOTAL: ${precio[2]}`)
+ let comprar = prompt("comprar S (O) E-salir");
+ if ( comprar != "E"){
+    function agregarRemeras(){
+        let cantidadRemeras = prompt("cuantas remeras quieres ?");
+        // let selecTalle = prompt("Escriba acá su tarea");
+        for(let i = 1; i <= cantidadRemeras; i++) {
+            let selecColor = prompt("seleccione color (1-roja) (2-negra) (3-azul)");
+                if(selecColor == "1"){
+                    console.log(`${remera}${color[0]}`)
+                }else if(selecColor == '2'){
+                    console.log(`${remera}${color[1]}`)
+                }else if (selecColor == '3'){
+                    console.log(`${remera}${color[2]}`)
                 }else {
-                    console.log("no eligio talle") 
+                    console.log("no eligio color") 
                 }
-            }else if( selecColor == "2"){
-                let selecTalle = prompt("seleccionar talle (1-M) (2-XL) (3-S)")
-                if(selecTalle == "1"){
-                    console.log(`${remera}${color[1]},TALLE: ${talle[0]},TOTAL: ${precio[0]}`)
-                }else if(selecTalle == '2'){
-                    console.log(`${remera}${color[1]},TALLE: ${talle[1]},TOTAL: ${precio[1]}`)
-                }else if (selecTalle == '3'){
-                    console.log(`${remera}${color[1]},TALLE: ${talle[2]},TOTAL: ${precio[2]}`)
-                }else {
-                    console.log("no eligio talle") 
-                }
-            }else if( selecColor == "3" ){
-                let selecTalle = prompt("seleccionar talle (1-M) (2-XL) (3-S)")
-                if(selecTalle == "1"){
-                    console.log(`${remera}${color[2]},TALLE: ${talle[0]},TOTAL: ${precio[0]}`)
-                }else if(selecTalle == '2'){
-                    console.log(`${remera}${color[2]},TALLE: ${talle[1]},TOTAL: ${precio[1]}`)
-                }else if (selecTalle == '3'){
-                    console.log(`${remera}${color[2]},TALLE: ${talle[2]},TOTAL: ${precio[2]}`)
-                }else {
-                    console.log("no eligio talle") 
-                }
+            let selecTalle = prompt("seleccionar talle (1-S) (2-M) (3-L) (4-XL) (5-XXL)")
+            if(selecTalle == "1"){
+                console.log(`TALLE: ${talle[0]},TOTAL: ${precio[0]}`)
+            }else if(selecTalle == '2'){
+                console.log(`,TALLE: ${talle[1]},TOTAL: ${precio[1]}`)
+            }else if (selecTalle == '3'){
+                console.log(`,TALLE: ${talle[2]},TOTAL: ${precio[2]}`)
+            }else if(selecTalle == '4'){
+                console.log(`,TALLE: ${talle[1]},TOTAL: ${precio[1]}`)
+            }else if (selecTalle == '5'){
+                console.log(`,TALLE: ${talle[2]},TOTAL: ${precio[2]}`)
+            }else {
+                console.log("no eligio talle") 
             }
+            toDoList.push(selecTalle);
+        }
+        
     }
-    if( selecColor == "E"){
-        salio = prompt("por que sale ?"); 
-        console.log(`el usuario respondio: ${salio} ,salio con exito`);   
-        aler1
+}else{   
+    function agregarRemeras(){
+        salio = prompt("por que sale ?");     
+        console.log(`el usuario respondio: ${salio} ,salio con exito` ); 
+      };
     }
-}else{
-    console.log(aler1)
-}
 
-}
-let aler1 = alert( "usted salio con exito");
+const toDoList = []
+            
+agregarRemeras();
+
